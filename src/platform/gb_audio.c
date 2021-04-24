2fdda7fefd39b9b66ec8520ec651b127c37adcf2
JaceCear <11230293+JaceCear@users.noreply.github.com>
2021-04-24 03:02:37 +0200

    Get audio sweeps in a working state

--- src/platform/gb_audio.c
+++ src/platform/gb_audio.c
@@ -27,19 +27,121 @@ static s32 currentSampleIndex[GB_TONE_CHANNEL_COUNT][AUDIO_CHANNEL_COUNT] = { 0 
 static s32 waveStart[GB_TONE_CHANNEL_COUNT][AUDIO_CHANNEL_COUNT] = { 0 };
 static AudioSample prevSampleValue[GB_TONE_CHANNEL_COUNT][AUDIO_CHANNEL_COUNT] = { 0 };
 
+static s32 sweepStart[AUDIO_CHANNEL_COUNT] = { 0 };
+
+static float sweepStartFrequency = 0.0f;
+static float sweepProgress[AUDIO_CHANNEL_COUNT] = { 0.0f };
+
+static float GetNthSweepFreq(float startFreq, int n, bool32 isDecrementing)
+{
+    float newFreq = startFreq;
+
+    while (n > 0) // TODO: Should this be > or >= ?
+    {
+        float diffFreq = newFreq / (float)(1 << n);
+
+        newFreq += isDecrementing ? -diffFreq : diffFreq;
+
+        n--;
+    }
+
+    return newFreq;
+}
+
+static s32 sweepRemainingSamples[AUDIO_CHANNEL_COUNT] = {0};
+
+static void
+ApplySweep(AudioSample *outBuffer, u16 sampleRate, u16 samplesPerFrame, float prevFreq, AudioSample volume, int audioChannel)
+{
+    AudioSample* out = outBuffer;
+    float currFreq = prevFreq;
+
+    const MsPerFrame = 1.0f / 60.0f;
+
+    u16 sweepReg = REG_SOUND1CNT_L;
+
+    int sweepCount = sweepReg & 0x7;
+    bool32 isDecrementSweep = sweepReg & (1 << 3);
+    
+    // Sample count and time each of the sweeps (*sweepCount) will take; every frequency step will take this same amount of time
+    float secondsPerSweep = (float)((sweepReg >> 4) & 0x7) * 0.0078f;
+    s32 samplesPerSweep = (float)sampleRate * secondsPerSweep;
+
+    bool32 sweepInit = (REG_SOUND1CNT_X & 0x8000);
+
+    if (sweepInit)
+    {
+        printf("Chan1: Sweep-Initted.\n");
+        REG_SOUND1CNT_X &= ~0x8000;
+
+        currFreq = 131072.0f / (2048.0f - (float)(REG_SOUND1CNT_X & 0x7FF));
+
+        // TODO: Reset everything that is needed for the previously playing sound
+        // so that the new settings can be applied
+        waveStart[0][0] = 0;
+        waveStart[0][1] = 0;
+        currentSampleIndex[0][0] = 0;
+        currentSampleIndex[0][1] = 0;
+        prevSampleValue[0][0] = 0;
+        prevSampleValue[0][1] = 0;
+
+        sweepRemainingSamples[0] = samplesPerSweep * (REG_SOUND1CNT_L & 0x7);
+        sweepRemainingSamples[1] = samplesPerSweep * (REG_SOUND1CNT_L & 0x7);
+    }
+    printf("%04X\n", sweepReg);
+    s32 initialSampleIndex = currentSampleIndex[0][audioChannel];
+    s32 remainingLoopSamples;
+    do
+    {
+        currFreq = GetNthSweepFreq(prevFreq, currentSampleIndex[0][audioChannel] / samplesPerSweep, isDecrementSweep);
+
+        s32 wavePeriod = sampleRate / currFreq;
+        u8 channelDuty = (REG_SOUND1CNT_H >> 6) & 0x3;
+        s32 waveDuty = wavePeriod * GbWaveDuty[channelDuty];
+
+        remainingLoopSamples = (currentSampleIndex[0][audioChannel] - initialSampleIndex);
+
+        s32 outputSampleCount = min(samplesPerSweep, samplesPerFrame);
+
+        if((remainingLoopSamples > 0) && remainingLoopSamples < outputSampleCount)
+            outputSampleCount = remainingLoopSamples;
+
+        //printf("samples: %d ; %d\n", samplesPerSweep, outputSampleCount);
+        //printf("freq   : %f\n", currFreq);
+
+
+        for (int regionSampleIndex = (currentSampleIndex[0][audioChannel] % outputSampleCount); regionSampleIndex < outputSampleCount; regionSampleIndex++, out++)
+        {
+            AudioSample SampleValue = (((waveStart[0][audioChannel] + currentSampleIndex[0][audioChannel]) % wavePeriod) < waveDuty) ? volume : -volume;
+
+            if (prevSampleValue[0][audioChannel] <= 0 && SampleValue > 0)
+                waveStart[0][audioChannel] = currentSampleIndex[0][audioChannel];
+
+            //printf("%c", (SampleValue > 0) ? '-' : '_');
+
+            out[audioChannel * MIXED_AUDIO_BUFFER_SIZE] += SampleValue;
+            prevSampleValue[0][audioChannel] = SampleValue;
+            currentSampleIndex[0][audioChannel]++;
+
+            if(--sweepRemainingSamples[audioChannel] == 0)
+                return;
+        }
+    } while (((out - outBuffer) / sizeof(AudioSample)) > samplesPerFrame);
+}
+
 void
-GbTest(s32* outBuffer, s32 sampleRate, u16 samplesPerFrame)
+GbTest(AudioSample* outBuffer, s32 sampleRate, u16 samplesPerFrame)
 {
     // TODO: Implement channel 1 sweeps
+    //       Properly implement envelope of both 1 and 2
     for (int gbChannel = 0; gbChannel < GB_TONE_CHANNEL_COUNT; gbChannel++)
     {
-        u16 sndxcnt_sweep = (gbChannel == 0) ? REG_SOUND1CNT_L : 0;
         u16 sndxcnt_LenEnvelope = (gbChannel == 0) ? REG_SOUND1CNT_H : REG_SOUND2CNT_L;
         u16 sndxcnt_freqCtrl = (gbChannel == 0) ? REG_SOUND1CNT_X : REG_SOUND2CNT_H;
 
         for (int audioChannel = 0; audioChannel < AUDIO_CHANNEL_COUNT; audioChannel++)
         {
-            s32* out = outBuffer;
+            AudioSample* out = outBuffer;
 
             bool32 isChannelEnabled = REG_SOUNDCNT_L & (1 << (8 + (audioChannel * 4) + gbChannel));
             if (isChannelEnabled) {
@@ -49,17 +151,27 @@ GbTest(s32* outBuffer, s32 sampleRate, u16 samplesPerFrame)
                 
                 float masterVol = (float)((REG_SOUNDCNT_L >> (audioChannel * 4)) & 0x3) / 7.0f;
                 
-
                 float frequency = 131072.0f / (2048.0f - (float)(sndxcnt_freqCtrl & 0x7FF));
-                s32 volume = masterVol * channelVolume * (float)MAX_VOLUME;
+                AudioSample volume = masterVol * channelVolume * (float)MAX_VOLUME;
 
 
                 if (volume > 0)
                 {
-                    int wavePeriod = sampleRate / frequency;
+                    if (gbChannel == 0)
+                    {
+#if 1
+                        bool32 sweepIsActive = ((REG_SOUND1CNT_L >> 4) & 0x7);
+                        
+                        if (sweepIsActive) {
+                            ApplySweep(outBuffer, sampleRate, samplesPerFrame,
+                                frequency, volume, audioChannel);
 
-                    bool32 channelInit = (sndxcnt_freqCtrl & 0x8000);
-                    bool32 channelStopOnZeroLength = (sndxcnt_freqCtrl & 0x4000);
+                            continue;
+                        }
+#endif
+                    }
+
+                    int wavePeriod = sampleRate / frequency;
 
                     u8 channelDuty = (sndxcnt_LenEnvelope >> 6) & 0x3;
                     int waveDuty = wavePeriod * GbWaveDuty[channelDuty];
